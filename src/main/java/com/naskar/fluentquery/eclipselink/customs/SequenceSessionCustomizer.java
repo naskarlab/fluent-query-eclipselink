9cf122e2689f4fed5aeea15e9101f214a202bfb4
rafaeluchoa <rafael@naskar.com.br>
2019-08-12 14:28:14 -0300

    attach sources

--- src/main/java/com/naskar/fluentquery/eclipselink/customs/SequenceSessionCustomizer.java
+++ src/main/java/com/naskar/fluentquery/eclipselink/customs/SequenceSessionCustomizer.java
@@ -1,6 +1,7 @@
 package com.naskar.fluentquery.eclipselink.customs;
 
 import java.util.Collection;
+import java.util.Map;
 
 import org.eclipse.persistence.config.SessionCustomizer;
 import org.eclipse.persistence.sequencing.Sequence;
@@ -8,12 +9,15 @@ import org.eclipse.persistence.sessions.Session;
 
 public class SequenceSessionCustomizer implements SessionCustomizer {
 
-	@SuppressWarnings("unchecked")
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
 	public void customize(Session session) throws Exception {
 		
-		for(Sequence s : (Collection<Sequence>)session.getLogin().getSequences().values()) {
-			s.setPreallocationSize(1);
+		Map seqs = session.getLogin().getSequences();
+		if(seqs != null && !seqs.isEmpty()) {
+			for(Sequence s : (Collection<Sequence>)seqs.values()) {
+				s.setPreallocationSize(1);
+			}
 		}
 		
 		session.getLogin().getDefaultSequence().setPreallocationSize(1);
